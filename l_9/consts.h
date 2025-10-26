8180fad5bacc4b56d9b628a1b2a4c1a1d3644316
xlebff <liveri1112@gmail.com>
2025-10-26 23:05:15 +0300

    v3.9.5

---
+++ l_9/consts.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+namespace PunctuationMsg {
+    extern const std::string DOT;
+    extern const std::string COMMA;
+    extern const std::string COLON;
+    extern const std::string SPACE;
+    extern const std::string STICK;
+}
+
+namespace ErrorMsg {
+    extern const std::string ERROR_NAN_MSG;
+    extern const std::string ERROR_INCORRECT_RANGE_MSG;
+    extern const std::string ERROR_NEGOTIVE_MSG;
+    extern const std::string ERROR_NOT_FOUND_MSG;
+    extern const std::string ERROR_NOT_UNIQUE_MSG;
+}
+
+namespace RelatedMsg {
+    extern const std::string AND;
+}
